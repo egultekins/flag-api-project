9f71fb2961c622b89a9a08f4c1b2b915be4f1717
egultekins <new-email@example.com>
2025-01-16 14:12:12 +0300

    Fetch countries data from REST Countries API and dynamically generate country cards.

---
+++ script.js
@@ -0,0 +1,25 @@
+fetch("https://restcountries.com/v3.1/all")
+  .then((res) => res.json())
+  .then((data) => {
+    const container = document.querySelector(".countries-card-container");
+
+  
+    const countryCards = data.map((country) => {
+      return `
+        <div class="country">
+          <img src="${country.flags.svg}" alt="${country.name.common}">
+          <h3>${country.name.common}</h3>
+          <div class="infos">
+            <p><b>Population:</b> ${country.population.toLocaleString()}</p>
+            <p><b>Region:</b> ${country.region}</p>
+            <p><b>Capital:</b> ${country.capital ? country.capital[0] : "N/A"}</p>
+          </div>
+        </div>
+      `;
+    });
+
+    container.innerHTML = countryCards.join('');
+  })
+  .catch((error) => {
+    console.error("Error fetching country data:", error);
+  });
