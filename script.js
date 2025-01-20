a44d76344b84700c0d7355108b1c53946b141427
egultekins <new-email@example.com>
2025-01-20 16:46:40 +0300

    Api service

--- script.js
+++ script.js
@@ -2,13 +2,30 @@ fetch("https://restcountries.com/v3.1/all")
   .then((res) => res.json())
   .then((data) => {
     const container = document.querySelector(".countries-card-container");
+    const detailsContainer = document.querySelector(".countries-card-container-2");
+    const MainContent = document.querySelector(".country-details-cont");                                                
+    const backButton = document.querySelector("a.back.button");
+    const flag = document.querySelector(".country-flag");
+    const name = document.querySelector(".country-name");
+    const native = document.querySelector(".data-native");
+    const population = document.querySelector(".country-population");
+    const region = document.querySelector(".country-region");
+    const subregion = document.querySelector(".country-subregion")
+    const capital = document.querySelector(".country-capital");
 
-  
+
+    // Render country cards
     const countryCards = data.map((country) => {
+        console.log(data)
       return `
-       
-        <div class="country">
-          <a href="country.html"  rel="noopener noreferrer" class="country-link">
+        <div class="country" 
+             data-name="${country.name.common}"
+             data-native="${country.name.official}" 
+             data-population="${country.population}" 
+             data-region="${country.region}" 
+             country-subregion="${country.status ? country.status : "N/A" }"
+             data-capital="${country.capital ? country.capital[0] : "N/A"}"
+             data-flag="${country.flags.svg}">
           <img src="${country.flags.svg}" alt="${country.name.common}">
           <h3>${country.name.common}</h3>
           <div class="infos">
@@ -16,17 +33,53 @@ fetch("https://restcountries.com/v3.1/all")
             <p><b>Region:</b> ${country.region}</p>
             <p><b>Capital:</b> ${country.capital ? country.capital[0] : "N/A"}</p>
           </div>
-          </a>
         </div>
-       
       `;
     });
 
     container.innerHTML = countryCards.join('');
+
+    // Add click event to each country card
+    container.addEventListener("click", (e) => {
+      const countryCard = e.target.closest(".country");
+      if (!countryCard) return;
+
+      // Get country details from data attributes
+      const countryName = countryCard.dataset.name;
+      const countryNative =countryCard.dataset.native;
+      const countryPopulation = countryCard.dataset.population;
+      const countryRegion = countryCard.dataset.region;
+      const countrySubRegion = countryCard.dataset.subregion;
+      const countryCapital = countryCard.dataset.capital;
+      const countryFlag = countryCard.dataset.flag;
+
+  
+
+      // Populate details container
+      flag.src = countryFlag;
+      name.textContent = countryName;
+      native.textContent = countryNative;
+      population.textContent = parseInt(countryPopulation).toLocaleString();
+      subregion.textContent = countrySubRegion;
+      region.textContent = countryRegion;
+      capital.textContent = countryCapital;
+
+     
+
+      // Show details container and hide list
+      container.style.display = "none";
+      detailsContainer.style.display = "block";
+      MainContent.style.display = "flex"
+    });
+
+    // Back button functionality
+    backButton.addEventListener("click", (e) => {
+      e.preventDefault(); // Prevent default anchor behavior
+      detailsContainer.style.display = "none";
+      MainContent.style.display = "none";
+      container.style.display = "flex";
+    });
   })
   .catch((error) => {
     console.error("Error fetching country data:", error);
   });
-
-
-
